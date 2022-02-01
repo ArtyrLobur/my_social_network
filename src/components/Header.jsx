afb150018babf2c16b233a3f08dd6292a2f61949
Artyr <enaken16@gmail.com>
2022-02-01 16:09:08 +0200

    start project

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return  <header className="header">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/2560px-Adidas_Logo.svg.png" />
+  </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Profile = () => {
+    return  <div className="content">
+    <div className="content-bar-img">
+      <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"></img>
+    </div>
+    <div> ava + description </div>
+    <div>
+      {" "}
+      My post
+      <div> New post </div>
+    </div>
+    <div>
+      <div>Post1</div>
+      <div>Post2</div>
+    </div>
+  </div>
+}
+
+export default Profile;
