0a6973589211b18f54dbedcba0b37813e36d6d98
Artyr <enaken16@gmail.com>
2022-02-02 12:42:18 +0200

    Props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -12,10 +12,9 @@ const MyPosts = () => {
               </div> 
           </div>
         <div className={s.posts}>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
+          <Post message ='Hi, how are you?'/>
+          <Post message ="It's my first post"/>
+        
         </div>
       </div>
 }
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return <div 
     className={s.item}>
       <img className={s.postProfileLogo} src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"></img>
-      Post1
+      { props.message }
         <div>
           <span>Like</span>
         </div>
