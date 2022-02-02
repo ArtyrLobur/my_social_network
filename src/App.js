cb989d526545520781d36ebec1722090f8e4d900
Artyr <enaken16@gmail.com>
2022-02-02 15:07:59 +0200

    19 lesson, route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
+import Music from "./components/Music/Music";
 import Navbar from "./components/Navbar/Navbar";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
 import Profile from "./components/Profile/Profile";
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
 
-      <div className="app-wrapper-content">
-        <Dialogs />
-        {/* <Profile /> */}
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/dialogs" element={<Dialogs />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
