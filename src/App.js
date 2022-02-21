03d5eb82387ec604096bacc4450d149d878bcfb6
Artyr <enaken16@gmail.com>
2022-02-21 18:32:43 +0200

    lesson 30

--- src/App.js
+++ src/App.js
@@ -12,29 +12,27 @@ import Friends from "./components/Friends/Friends";
 
 const App = (props) => {
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <Header />
-        <Navbar />
-      
-        <div className="app-wrapper-content">
-            <Routes>
-              <Route
-                path="/profile"
-                element={<Profile state={props.state.profilePage} />}
-              />
-              <Route
-                path="/dialogs"
-                element={<Dialogs state={props.state.messagePage} />}
-              />
-              <Route path="/news" element={<News />} />
-              <Route path="/music" element={<Music />} />
-              <Route path="/settings" element={<Settings />} />
-              {/* <Route path="/friends" element={<Friends />} /> */}
-            </Routes>
-        </div>
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+
+      <div className="app-wrapper-content">
+        <Routes>
+          <Route
+            path="/profile"
+            element={<Profile state={props.state.profilePage} />}
+          />
+          <Route
+            path="/dialogs"
+            element={<Dialogs state={props.state.messagePage} />}
+          />
+          <Route path="/news" element={<News />} />
+          <Route path="/music" element={<Music />} />
+          <Route path="/settings" element={<Settings />} />
+          {/* <Route path="/friends" element={<Friends />} /> */}
+        </Routes>
       </div>
-    </BrowserRouter>
+    </div>
   );
 };
 
