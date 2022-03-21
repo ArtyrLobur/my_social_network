2a20dfba87aa8b632c5553a47488a926e1079271
ArtyrLobur <enaken16@gmail.com>
2022-03-21 17:28:18 +0200

    lesson 35 observer

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from "../render";
+let rerenderEntireTree = () => {
+  console.log('State was Change');
+}
 
 let state = {
   profilePage: {
@@ -33,7 +35,7 @@ let state = {
   },
 };
 
-export let addPost = () => {
+export const addPost = () => {
   let newPost = {
     id: 5,
     message: state.profilePage.newPostText,
@@ -46,9 +48,13 @@ export let addPost = () => {
   rerenderEntireTree(state);
 };
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
   state.profilePage.newPostText = newText;
   rerenderEntireTree(state);
 };
+
+export const subscribe = (observer) => {
+  rerenderEntireTree = observer; //observer
+}
 
 export default state;
