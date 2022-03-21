007cb0d32a750f6fc560debf714057156b2dde3c
ArtyrLobur <enaken16@gmail.com>
2022-03-21 18:55:52 +0200

    lesspn 37 store, state

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-import state, { subscribe } from "./redux/state";
+import store from "./redux/state";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import { addPost, updateNewPostText } from "./redux/state";
 
 let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <React.StrictMode> 
         <BrowserRouter>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+            <App state={state} 
+                addPost={store.addPost.bind(store)} 
+                updateNewPostText={store.updateNewPostText.bind(store)}/>
         </BrowserRouter> 
     </React.StrictMode>,
     document.getElementById("root")
   );
 };
 
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,60 +1,63 @@
-let rerenderEntireTree = () => {
-  console.log('State was Change');
-}
-
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: "Hi, how are you?", likeCout: "15 Likes" },
-      { id: 2, message: "It's my first post", likeCout: "7 Likes" },
-      { id: 3, message: "It's my second post", likeCout: "9 Likes" },
-      { id: 4, message: "It's my post", likeCout: "2 Likes" },
-    ],
-    newPostText: 'react lessons',
-  },
-
-  messagePage: {
-    messages: [
-      { id: 1, message: "Hi" },
-      { id: 2, message: "How are you?" },
-      { id: 3, message: "Yo" },
-      { id: 4, message: "My english is beautiful" },
-      { id: 5, message: "Privet Ded" },
-      { id: 6, message: "Nice car" },
-    ],
-
-    dialogs: [
-      { id: 1, name: "Dima" },
-      { id: 2, name: "Andrey" },
-      { id: 3, name: "Sveta" },
-      { id: 4, name: "Sasha" },
-      { id: 5, name: "Viktor" },
-      { id: 6, name: "Artyr" },
-    ],
-    sidebar: {},
-  },
-};
-
-export const addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likeCout: 0,
-  };
-
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hi, how are you?", likeCout: "15 Likes" },
+        { id: 2, message: "It's my first post", likeCout: "7 Likes" },
+        { id: 3, message: "It's my second post", likeCout: "9 Likes" },
+        { id: 4, message: "It's my post", likeCout: "2 Likes" },
+      ],
+      newPostText: 'react lessons',
+    },
   
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText='';
-  rerenderEntireTree(state);
-};
+    messagePage: {
+      messages: [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "How are you?" },
+        { id: 3, message: "Yo" },
+        { id: 4, message: "My english is beautiful" },
+        { id: 5, message: "Privet Ded" },
+        { id: 6, message: "Nice car" },
+      ],
+  
+      dialogs: [
+        { id: 1, name: "Dima" },
+        { id: 2, name: "Andrey" },
+        { id: 3, name: "Sveta" },
+        { id: 4, name: "Sasha" },
+        { id: 5, name: "Viktor" },
+        { id: 6, name: "Artyr" },
+      ],
+      sidebar: {},
+    },
+  },
 
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer; //observer
+  getState() {
+    return this._state
+  },
+  _callSubscriber () {
+    console.log('State was Change');
+  },
+  addPost () {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likeCout: 0,
+    };
+  
+    
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText='';
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText (newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe (observer) {
+    this._callSubscriber = observer; //observer
+  },
 }
 
-export default state;
+export default store;
+window.store = store;
