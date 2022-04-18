847ccf4b07e06e3e524843bb953c9c5d3bc7b30e
Artyr Lobur <enaken16@gmail.com>
2022-04-18 18:59:45 +0300

    lesson 41, reduce, if to switch case

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -3,23 +3,26 @@ const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 const profileReducer = (state, action) => {
     
-    if (action.type === ADD_POST) { 
-        let newPost = {
-          id: 5,
-          message: state.newPostText,
-          likeCout: 0,
-        };
+    switch (action.type) { 
+        case ADD_POST:
+            let newPost = {
+            id: 5,
+            message: state.newPostText,
+            likeCout: 0,
+            };
         
-        state.posts.push(newPost);
-        state.newPostText='';
-      } 
-      
-      else if (action.type === UPDATE_NEW_POST_TEXT) {
-        state.newPostText = action.newText;
-      } 
+            state.posts.push(newPost);
+            state.newPostText='';
+            return state;
 
-    return state;
-} 
+        case UPDATE_NEW_POST_TEXT:
+            state.newPostText = action.newText;
+            return state;
+
+        default:
+            return state;
+    } 
+}
 
 export const addPostActionCreator = () => ({ type: ADD_POST})
 export const updateNewPostTextActionCreator = (text) => ({ type: UPDATE_NEW_POST_TEXT, newText: text })
