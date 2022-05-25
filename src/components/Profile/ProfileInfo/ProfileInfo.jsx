d0337cea4cbfcfc57c5f407037a3c540f181a218
Artyr Lobur <enaken16@gmail.com>
2022-05-25 19:51:49 +0300

    51 API - get

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -5,7 +5,7 @@ const ProfileInfo = () => {
     return  (
     <div>
       <div className={s.content_bar_img}>
-        <img src="https://www.iphones.ru/wp-content/uploads/2021/09/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA-%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0-2021-09-07-%D0%B2-19.48.05.jpg"></img>
+        <img src="https://www.iphones.ru/wp-content/uploads/2021/09/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA-%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0-2021-09-07-%D0%B2-19.48.05.jpg" alt="logo"></img>
       </div>
       <div className={s.descriptionBlock}> ava + description </div>
     </div>
